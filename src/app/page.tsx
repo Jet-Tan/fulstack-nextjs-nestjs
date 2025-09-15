0be8f28f1450addb7b8a3fc3339216934c60fa06
unknown <truongngoctan2388@gmail.com>
2025-09-15 22:00:30 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "antd";
+
 export default function Home() {
   return (
-  <div>Hello world</div>
+    <div>
+      Hello world
+      <div>
+        <Button>Add new</Button>
+      </div>
+    </div>
   );
 }
